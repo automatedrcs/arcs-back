508f223086adddf21340e26837abac323f1b6f2c
Junah <junah@Nathans-MacBook-Pro-7.local>
2023-09-03 12:29:27 -0700

    get_secret duplicate in main

--- src/main.py
+++ src/main.py
@@ -34,10 +34,3 @@ async def shutdown():
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-def get_secret(project_id, secret_id, version_id="latest"):
-    client = secretmanager.SecretManagerServiceClient()
-    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(request={"name": secret_name})
-    return response.payload.data.decode('UTF-8')
