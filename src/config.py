3db9668ee0791677f13306bfdac52c98c68474de
Junah Park <park.junah95@gmail.com>
2023-09-06 05:22:20 +0000

    port number for sql

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ USERNAME = get_secret("arcs-391022", "arcs-db-username")
 PASSWORD = get_secret("arcs-391022", "arcs-db-password")
 HOST = "arcs-391022:us-central1:arcs-sql-instance"
 DBNAME = "arcs_db"
-PORT = 8080
+PORT = 5432
 
 # Construct the DATABASE_URL
 DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
