251642ccea8fd81d3dd2e7b74e4f7df9f6777bec
Junah Park <park.junah95@gmail.com>
2023-09-06 19:08:37 +0000

    cloudbuild.yaml revision

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ from .utils import get_secret
 USERNAME = get_secret("arcs-391022", "arcs-db-username")
 PASSWORD = get_secret("arcs-391022", "arcs-db-password")
 HOST = "34.171.204.136"
-DBNAME = "arcs_db"
-PORT = "5432"
+DBNAME = "arcs-db"
+PORT = 5432
 
 # Construct the DATABASE_URL
 DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
