3126f37b5fb0d0bd778644d67b1c0f59d00b8f8c
Junah Park <park.junah95@gmail.com>
2023-09-06 06:38:30 +0000

    arcs db connection

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ from .utils import get_secret
 
 USERNAME = get_secret("arcs-391022", "arcs-db-username")
 PASSWORD = get_secret("arcs-391022", "arcs-db-password")
-HOST = "arcs-391022:us-central1:arcs-sql-instance"
+HOST = "34.171.204.136"
 DBNAME = "arcs_db"
-PORT = 5432
+PORT = "5432"
 
 # Construct the DATABASE_URL
 DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
