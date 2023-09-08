104052fb83b9a9ebad5f258b671c2097a18c9a74
Junah Park <park.junah95@gmail.com>
2023-09-08 05:23:25 +0000

    get db username and pw

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 # This will hold the configurations for your application
 from .utils import get_secret
 
-USERNAME = get_secret("arcs-391022", "arcs-db-username")
-PASSWORD = get_secret("arcs-391022", "arcs-db-password")
+USERNAME = get_secret("arcs-db-username")
+PASSWORD = get_secret("arcs-db-password")
 HOST = "10.223.0.8"
 DBNAME = "arcs-db"
 PORT = 5432
