cd087fb8f20993429c55a8b5341fbfbd33967d9d
Junah <junah@Nathans-MacBook-Pro-7.local>
2023-09-14 23:51:46 -0700

    comma typo

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean, func, 
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean, func
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import relationship
 import uuid
