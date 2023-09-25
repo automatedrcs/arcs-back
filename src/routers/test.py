e6b13bd649a622bbe97c0d8e1e688c6d070e8e79
Junah Park <park.junah95@gmail.com>
2023-09-25 16:30:45 -0700

    db connection-test

--- src/routers/test.py
+++ src/routers/test.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from database import models, database
 
 test_router = APIRouter()
 
@@ -10,3 +14,23 @@ def get_data():
             "example_key": "example_value"
         }
     }
+
+@test_router.get("/api/connection-test")
+async def test_connection(db: AsyncSession = Depends(database.get_db)):
+    try:
+        # Attempt to fetch the first row from the Organization table
+        result = await db.execute(select(models.Organization).order_by(models.Organization.id).limit(1))
+        organization = result.fetchone()
+        if organization:
+            return {
+                "message": "Connection successful. Data fetched successfully.",
+                "data": {
+                    "organization_name": organization.name,
+                    "organization_data": organization.data
+                }
+            }
+        # If there is no data in the table
+        return {"message": "Connection successful. No data found in the table.", "data": {}}
+    except Exception as e:
+        # If there's an error related to database connection or query execution
+        raise HTTPException(status_code=500, detail=str(e))
