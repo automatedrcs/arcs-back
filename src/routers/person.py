5b92f539d48ffdcba6e127238b4c4c24bed383bb
Junah Park <park.junah95@gmail.com>
2023-10-05 16:18:02 -0700

    person model is not personmodel

--- src/routers/person.py
+++ src/routers/person.py
@@ -6,8 +6,8 @@ from database import models, schema, database
 
 # ------------------------- CRUD Operations -------------------------
 
-def create_person(db: Session, person: schema.PersonCreate) -> models.PersonModel:
-    db_person = models.PersonModel(**person.dict())
+def create_person(db: Session, person: schema.PersonCreate) -> models.Person:
+    db_person = models.Person(**person.dict())
     db.add(db_person)
     db.commit()
     db.refresh(db_person)
@@ -21,25 +21,25 @@ def get_people(
         role: Optional[str] = None,
         skip: int = 0,
         limit: int = 100
-) -> Union[models.PersonModel, List[models.PersonModel]]:
-    query = db.query(models.PersonModel)
+) -> Union[models.Person, List[models.Person]]:
+    query = db.query(models.Person)
 
     if person_id:
-        return query.filter(models.PersonModel.id == person_id).first()
+        return query.filter(models.Person.id == person_id).first()
 
     if organization_id:
-        query = query.filter(models.PersonModel.organization_id == organization_id)
+        query = query.filter(models.Person.organization_id == organization_id)
         
     if user_id:
-        query = query.filter(models.PersonModel.user_id == user_id)
+        query = query.filter(models.Person.user_id == user_id)
 
     if role:
-        query = query.filter(models.PersonModel.role == role)
+        query = query.filter(models.Person.role == role)
 
     return query.offset(skip).limit(limit).all()
 
-def update_person(db: Session, person_id: UUID, person: schema.PersonUpdate) -> Optional[models.PersonModel]:
-    db_person = db.query(models.PersonModel).filter(models.PersonModel.id == person_id).first()
+def update_person(db: Session, person_id: UUID, person: schema.PersonUpdate) -> Optional[models.Person]:
+    db_person = db.query(models.Person).filter(models.Person.id == person_id).first()
     if db_person:
         for key, value in person.dict().items():
             setattr(db_person, key, value)
@@ -47,8 +47,8 @@ def update_person(db: Session, person_id: UUID, person: schema.PersonUpdate) -> 
         db.refresh(db_person)
     return db_person
 
-def delete_person(db: Session, person_id: UUID) -> Optional[models.PersonModel]:
-    db_person = db.query(models.PersonModel).filter(models.PersonModel.id == person_id).first()
+def delete_person(db: Session, person_id: UUID) -> Optional[models.Person]:
+    db_person = db.query(models.Person).filter(models.Person.id == person_id).first()
     if db_person:
         db.delete(db_person)
         db.commit()
@@ -59,7 +59,7 @@ def delete_person(db: Session, person_id: UUID) -> Optional[models.PersonModel]:
 person_router = APIRouter()
 
 @person_router.post("", response_model=schema.Person)
-def create_new_person(person: schema.PersonCreate, db: Session = Depends(database.get_db)) -> models.PersonModel:
+def create_new_person(person: schema.PersonCreate, db: Session = Depends(database.get_db)) -> models.Person:
     return create_person(db, person)
 
 @person_router.get("/organization/{organization_id}/person/{person_id}", response_model=schema.Person)
@@ -83,14 +83,14 @@ def read_people(
 
 @person_router.put("/{person_id}", response_model=schema.Person)
 def update_existing_person(person_id: UUID, person: schema.PersonUpdate, db: Session = Depends(database.get_db)):
-    db_person = db.query(models.PersonModel).filter(models.PersonModel.id == person_id).first()
+    db_person = db.query(models.Person).filter(models.Person.id == person_id).first()
     if not db_person:
         raise HTTPException(status_code=404, detail="Person not found")
     return update_person(db, person_id, person)
 
 @person_router.delete("/{person_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_a_person(person_id: UUID, db: Session = Depends(database.get_db)):
-    db_person = db.query(models.PersonModel).filter(models.PersonModel.id == person_id).first()
+    db_person = db.query(models.Person).filter(models.Person.id == person_id).first()
     if not db_person:
         raise HTTPException(status_code=404, detail="Person not found")
     delete_person(db, person_id)
