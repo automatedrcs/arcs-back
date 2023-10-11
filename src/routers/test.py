ed4a7ffd1fc89cccd3d4187f438a6a7ece97141e
Junah Park <park.junah95@gmail.com>
2023-10-11 12:27:04 -0700

    api connection test for calendar data

--- src/routers/test.py
+++ src/routers/test.py
@@ -1,34 +1,55 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import models, database
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from utils import decrypt
+import requests
 
 test_router = APIRouter()
 
-@test_router.get("/api/data")
-def get_data():
-    return {
-        "message": "Data fetched successfully",
-        "data": {
-            "example_key": "example_value"
-        }
+def refresh_google_token(refresh_token: str) -> dict:
+    client_id = get_secret("CLIENT_ID")
+    client_secret = get_secret("CLIENT_SECRET")
+
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'refresh_token': decrypt(refresh_token),
+        'grant_type': 'refresh_token'
     }
+
+    r = requests.post('https://oauth2.googleapis.com/token', data=data)
+    return r.json()
 
 @test_router.get("/api/connection-test")
 def test_connection(db: Session = Depends(database.get_db)):
     try:
-        # Attempt to fetch the first row from the Organization table
-        result = db.query(models.Organization).order_by(models.Organization.id).first()
+        # Attempt to fetch the first row from the User table
+        user = db.query(models.User).order_by(models.User.id).first()
         
-        if result:
-            return {
-                "message": "Connection successful. Data fetched successfully.",
-                "data": {
-                    "organization_name": result.name,
-                    "organization_data": result.data
-                }
+        if not user:
+            return {"message": "Connection successful. No user found in the table.", "data": {}}
+
+        if "authentication" not in user.data or "google" not in user.data["authentication"] or "refresh_token" not in user.data["authentication"]["google"]:
+            return {"message": "Connection successful. No Google Refresh Token found for the user.", "data": {}}
+
+        refreshed_token = refresh_google_token(user.data["authentication"]["google"]["refresh_token"])
+        access_token = refreshed_token['access_token']
+
+        credentials = Credentials(token=access_token)
+        service = build("calendar", "v3", credentials=credentials)
+
+        events_result = service.events().list(calendarId='primary', timeMax='2100-01-01T00:00:00Z', maxResults=1, singleEvents=True, orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        return {
+            "message": "Successful calendar connection. Data fetched successfully.",
+            "data": {
+                "first_event": events[0] if events else "No events found in the calendar.",
+                "refresh_token": user.data["authentication"]["google"]["refresh_token"]
             }
-        # If there is no data in the table
-        return {"message": "Connection successful. No data found in the table.", "data": {}}
+        }
     except Exception as e:
-        # If there's an error related to database connection or query execution
+        # If there's an error related to database connection, API call, or query execution
         raise HTTPException(status_code=500, detail=str(e))
