636b9788b41f589e37ba380d8cabc1093e758150
Junah Park <park.junah95@gmail.com>
2023-10-17 23:01:00 -0700

    testing datetime formats

--- src/routers/calendar.py
+++ src/routers/calendar.py
@@ -50,20 +50,36 @@ async def fetch_google_calendar_events(db: Session, entity, start_time, end_time
     credentials = Credentials(token=refreshed_token['access_token'])
     service = build("calendar", "v3", credentials=credentials)
 
-    timeMin_formatted = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+     timeMin_formatted = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
     timeMax_formatted = end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    try:
-        events_result = service.events().list(
-            calendarId='primary',
-            timeMin=timeMin_formatted,
-            timeMax=timeMax_formatted,
-            singleEvents=True,
-            orderBy='startTime'
-        ).execute()
-        return events_result.get('items', [])
-    except googleapiclient.errors.HttpError as error:
-        raise HTTPException(status_code=500, detail="Error fetching Google Calendar events")
+    datetime_formats = [
+        ('Original Format', '%Y-%m-%dT%H:%M:%SZ'),
+        ('SO Answer 1', '%Y-%m-%dT%H:%M:%S'),
+        ('SO Answer 2', '%Y-%m-%dT%H:%M:%S.%fZ'),
+        ('SO Answer 3 (only datetime)', '%Y-%m-%dT%H:%M:%S.%f'),
+    ]
+
+    for format_name, date_format in datetime_formats:
+        timeMin_test = start_time.strftime(date_format)
+        timeMax_test = end_time.strftime(date_format)
+        
+        print(f"Testing {format_name}: Start Time - {timeMin_test}, End Time - {timeMax_test}")
+        
+        try:
+            events_result = service.events().list(
+                calendarId='primary',
+                timeMin=timeMin_test,
+                timeMax=timeMax_test,
+                singleEvents=True,
+                orderBy='startTime'
+            ).execute()
+            print(f"{format_name} - SUCCESS!")
+            return events_result.get('items', [])
+        except googleapiclient.errors.HttpError as error:
+            print(f"{format_name} - FAILED with error: {error}")
+
+    raise HTTPException(status_code=500, detail="All date formats failed for Google Calendar events")
 
 
 @calendar_router.get("/events/user")
