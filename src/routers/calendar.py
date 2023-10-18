70d847cefbb2791fe54b2e09c59efeda7a5ee97e
Junah Park <park.junah95@gmail.com>
2023-10-17 23:44:37 -0700

    date time format

--- src/routers/calendar.py
+++ src/routers/calendar.py
@@ -14,13 +14,11 @@ from utils import decrypt, get_secret
 
 calendar_router = APIRouter()
 
-
 def get_user_by_id(db: Session, user_id: str) -> models.User:
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
-
 
 def refresh_google_token(refresh_token: str) -> dict:
     client_id = get_secret("CLIENT_ID")
@@ -36,7 +34,6 @@ def refresh_google_token(refresh_token: str) -> dict:
     response = requests.post('https://oauth2.googleapis.com/token', data=data)
     return response.json()
 
-
 async def fetch_google_calendar_events(db: Session, entity, start_time, end_time):
     if "authentication" not in entity.data or "google" not in entity.data["authentication"] or "refresh_token" not in entity.data["authentication"]["google"]:
         raise HTTPException(status_code=403, detail="Refresh Token required for Google Calendar operations")
@@ -50,34 +47,20 @@ async def fetch_google_calendar_events(db: Session, entity, start_time, end_time
     credentials = Credentials(token=refreshed_token['access_token'])
     service = build("calendar", "v3", credentials=credentials)
 
-    datetime_formats = [
-        ('Original Format', '%Y-%m-%dT%H:%M:%SZ'),
-        ('SO Answer 1', '%Y-%m-%dT%H:%M:%S'),
-        ('SO Answer 2', '%Y-%m-%dT%H:%M:%S.%fZ'),
-        ('SO Answer 3 (only datetime)', '%Y-%m-%dT%H:%M:%S.%f'),
-    ]
+    timeMin_formatted = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    timeMax_formatted = end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    for format_name, date_format in datetime_formats:
-        timeMin_test = start_time.strftime(date_format)
-        timeMax_test = end_time.strftime(date_format)
-        
-        print(f"Testing {format_name}: Start Time - {timeMin_test}, End Time - {timeMax_test}")
-        
-        try:
-            events_result = service.events().list(
-                calendarId='primary',
-                timeMin=timeMin_test,
-                timeMax=timeMax_test,
-                singleEvents=True,
-                orderBy='startTime'
-            ).execute()
-            print(f"{format_name} - SUCCESS!")
-            return events_result.get('items', [])
-        except googleapiclient.errors.HttpError as error:
-            print(f"{format_name} - FAILED with error: {error}")
-
-    raise HTTPException(status_code=500, detail="All date formats failed for Google Calendar events")
-
+    try:
+        events_result = service.events().list(
+            calendarId='primary',
+            timeMin=timeMin_formatted,
+            timeMax=timeMax_formatted,
+            singleEvents=True,
+            orderBy='startTime'
+        ).execute()
+        return events_result.get('items', [])
+    except googleapiclient.errors.HttpError as error:
+        raise HTTPException(status_code=500, detail="Error fetching Google Calendar events")
 
 @calendar_router.get("/events/user")
 async def get_user_calendar_events(
